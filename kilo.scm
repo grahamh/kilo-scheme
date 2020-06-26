eb3a749299ec90bf76c74240fbc2e3080634252e
graham <graham@garden-images.co.uk>
2020-06-26 22:43:05 +0100

    Step 5

--- kilo.scm
+++ kilo.scm
@@ -1,4 +1,9 @@
+(require-extension stty)
 
+(define (enable-raw-mode)
+  (stty '(not echo)))
+
+(enable-raw-mode)
 (do ((c (read-char) (read-char)))
     ((or (eof-object? c) (char=? c #\q)) 
      (exit 0))
